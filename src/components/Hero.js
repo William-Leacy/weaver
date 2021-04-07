871d498e3f65a307dff533fa18db8e8b7cd67449
William Leacy <will.leacy10@gmail.com>
2021-04-07 12:02:09 -0400

    Add boiler plate auth0 installtion

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import logo from "../assets/logo.svg";
+
+const Hero = () => (
+  <div className="text-center hero my-5">
+    <h1 className="mb-4">Welcome to weaver</h1>
+
+    <p className="lead">
+      Weaver is an image editor web application for weaving images together
+    </p>
+  </div>
+);
+
+export default Hero;
