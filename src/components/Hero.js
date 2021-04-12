6dc109b2f58971fdf7575555d9cc4f0797fb7dcb
William Leacy <will.leacy10@gmail.com>
2021-04-12 12:25:25 -0400

    ADd update app weaver h1 tag on teh home page to use google font

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import logo from "../assets/logo.svg";
 
 const Hero = () => (
   <div className="text-center hero my-5">
-    <h1 className="mb-4">Welcome to weaver</h1>
+    <h1 className="app-tilte ">Weaver</h1>
 
     <p className="lead">
       Weaver is an image editor web application for weaving images together
